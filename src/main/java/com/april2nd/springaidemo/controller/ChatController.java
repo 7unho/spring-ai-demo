5d7b12a9b91f38bf5f6255f983010bebe45374ac
7unho <rlawnsgh8395@naver.com>
2026-09-16 22:36:12 +0900

    Feat: `/api/ai/chat`, `/api/ai/stream` api 작성

--- src/main/java/com/april2nd/springaidemo/controller/ChatController.java
+++ src/main/java/com/april2nd/springaidemo/controller/ChatController.java
@@ -1,9 +1,40 @@
 package com.april2nd.springaidemo.controller;
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.prompt.ChatOptions;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @RestController
 @RequestMapping("/api/ai")
 public class ChatController {
+    private final ChatClient chatClient;
+
+    public ChatController(ChatClient.Builder builder) {
+        this.chatClient = builder.build();
+    }
+
+    @GetMapping("/ask")
+    public String ask(@RequestParam String question) {
+        return chatClient.prompt()
+                .options(
+                        ChatOptions.builder()
+                                .temperature(0.1)
+                )
+                .user(question)
+                .call()
+                .content();
+    }
+
+    @GetMapping(value = "/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> stream(@RequestParam String question) {
+        return chatClient.prompt()
+                .user(question)
+                .stream()
+                .content();
+    }
 }
--- src/test/java/com/april2nd/springaidemo/service/OpenAIServiceImplTest.java
+++ src/test/java/com/april2nd/springaidemo/service/OpenAIServiceImplTest.java
@@ -14,8 +14,8 @@ class OpenAIServiceImplTest {
     void getAnswer() {
 //        String answer = openAIService.getAnswer("What is the meaning of life?");
 //        String answer = openAIService.getAnswer("생명의 의미는 무엇인가?");
-        String answer = openAIService.getAnswer("Wrtie a python script to output numbers from 1 to 100");
+//        String answer = openAIService.getAnswer("Wrtie a python script to output numbers from 1 to 100");
 
-        System.out.println(answer);
+//        System.out.println(answer);
     }
 }
